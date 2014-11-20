d2e3829b9dbc1165abfa71decd10cc5fe2c06c87
Cody Benner <strykstaguy@gmail.com>
2014-11-19 23:51:55 -0600

    fixed events dialog

--- app/build/generated/source/r/debug/com/afollestad/materialdialogs/R.java
+++ app/build/generated/source/r/debug/com/afollestad/materialdialogs/R.java
@@ -496,7 +496,7 @@ public final class R {
 		public static final int md_listitem = 0x7f030040;
 		public static final int md_listitem_multichoice = 0x7f030041;
 		public static final int md_listitem_singlechoice = 0x7f030042;
-		public static final int support_simple_spinner_dropdown_item = 0x7f030050;
+		public static final int support_simple_spinner_dropdown_item = 0x7f030051;
 	}
 	public static final class string {
 		public static final int abc_action_bar_home_description = 0x7f0c0000;
--- app/src/main/java/com/stryksta/swtorcentral/EventsActivity.java
+++ app/src/main/java/com/stryksta/swtorcentral/EventsActivity.java
@@ -1,5 +1,6 @@
 package com.stryksta.swtorcentral;
  
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +11,10 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Alignment;
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.Theme;
+
 import com.stryksta.swtorcentral.data.EventItem;
 import com.stryksta.swtorcentral.util.Utility;
 
@@ -62,6 +67,14 @@ public class EventsActivity extends ActionBarActivity {
             super.onPreExecute();
             // Create a progressbar
             //pDialog  = new MaterialDialog.Builder(EventsActivity.this);
+            //MaterialDialog dialog = new MaterialDialog.Builder(EventsActivity.this);
+            pDialog = new MaterialDialog.Builder(EventsActivity.this)
+                    .title("")
+                    .customView(R.layout.progress_alert)
+                    .positiveText("")
+                    .hideActions()
+                    .build();
+            pDialog.show();
             //MaterialDialog pDialog new MaterialDialog.Builder(this);
             //pDialog.setMessage("Loading...");
             //pDialog.setIndeterminate(false);
@@ -158,7 +171,7 @@ public class EventsActivity extends ActionBarActivity {
             EventAdapter eventsAdapter = new EventAdapter(EventsActivity.this, R.layout.event_row, eventsItems);
             eventsGridView.setAdapter(eventsAdapter);
 
-            pDialog.dismiss();
+            //pDialog.dismiss();
         }
     }
     @Override
