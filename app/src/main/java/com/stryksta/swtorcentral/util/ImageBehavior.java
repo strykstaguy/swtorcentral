ecc0af79ddc2c988ad6aa723850467fa3c7239d6
Cody <strykstaguy@gmail.com>
2016-02-29 14:31:38 -0600

    update planet ui

---
+++ app/src/main/java/com/stryksta/swtorcentral/util/ImageBehavior.java
@@ -0,0 +1,27 @@
+package com.stryksta.swtorcentral.util;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ImageBehavior extends CoordinatorLayout.Behavior<View> {
+
+
+    public ImageBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        child.setY(dependency.getY()-170);
+        return true;
+    }
+}
