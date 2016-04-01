2d20b224140d634d9b9a8e14f4228f485dc9416b
Cody <strykstaguy@gmail.com>
2016-03-31 20:52:13 -0500

    implementing companions activity

---
+++ app/src/main/java/com/stryksta/swtorcentral/CompanionsActivity.java
@@ -0,0 +1,49 @@
+package com.stryksta.swtorcentral;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class CompanionsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.companions_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        getSupportActionBar().setTitle("Companions");
+        
+     // Debug the thread name
+     	Log.d("SWTORCentral", Thread.currentThread().getName());
+        
+    }
+    
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    // Respond to the action bar's Up/Home button
+	    case android.R.id.home:
+	    	this.finish();
+	        return true;
+	    }
+	    return super.onOptionsItemSelected(item);
+	}
+	
+	public void onBackPressed() {
+		if (getFragmentManager().getBackStackEntryCount() == 0) {
+	        this.finish();
+	    } else {
+	        getFragmentManager().popBackStack();
+	    }
+    }
+}
--- app/src/main/java/com/stryksta/swtorcentral/adapters/CompanionAdapter.java
+++ app/src/main/java/com/stryksta/swtorcentral/adapters/CompanionAdapter.java
@@ -1,91 +1,53 @@
 package com.stryksta.swtorcentral.adapters;
 
-import android.content.Context;
-import android.database.DatabaseUtils;
-import android.view.LayoutInflater;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.stryksta.swtorcentral.R;
+import com.stryksta.swtorcentral.data.AbilitiesItem;
 import com.stryksta.swtorcentral.data.CompanionItem;
 
 import java.util.ArrayList;
 
-public class CompanionAdapter extends ArrayAdapter<CompanionItem> {
+public class CompanionsAdapter extends RecyclerView.Adapter<CompanionsAdapter.ViewHolder>{
 
-	Context mContext;
-    private final ArrayList<CompanionItem> results;
-    public CompanionAdapter(Context mContext, final int textViewResourceId,
-            final ArrayList<CompanionItem> results) {
+    private ArrayList<CompanionItem> companionItem;
 
-        super(mContext, textViewResourceId, results);
-        this.results = results;
-        this.mContext = mContext;
-    }
-
-    public String unescapeString(String string) {
-        if (string == null)
-            return null;
-        
-        string = DatabaseUtils.sqlEscapeString(string);
-        @SuppressWarnings("StringBufferReplaceableByString") StringBuilder sb = new StringBuilder(string);
-
-        return sb.toString().replaceAll("\\\n", "\n");
+    public CompanionsAdapter(ArrayList<CompanionItem> companionItem) {
+        super();
+        this.companionItem = companionItem;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        View v = convertView;
-        ViewHolder holder;
-
-        if (v == null) {
-            LayoutInflater inflater = (LayoutInflater) mContext
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            //v = inflater.inflate(R.layout.advanced_class_row, null);
-            holder = new ViewHolder();
-            
-            holder.txtName = (TextView) v.findViewById(R.id.txtName);
-            //holder.txtNameSub = (TextView) v.findViewById(R.id.txtNameSub);
-            holder.txtRole = (TextView) v.findViewById(R.id.txtRole);
-            //holder.txtBonus = (TextView) v.findViewById(R.id.txtBonus);
-            //holder.txtRomance = (TextView) v.findViewById(R.id.txtRomance);
-            //holder.txtStats = (TextView) v.findViewById(R.id.txtStats);
-            //holder.txtWeapon = (TextView) v.findViewById(R.id.txtWeapon);
-            holder.txtDescription = (TextView) v.findViewById(R.id.txtDescription);
-            holder.txtArmor = (TextView) v.findViewById(R.id.txtArmor);
-            
-            v.setTag(holder);
-        } else {
-            holder = (ViewHolder) v.getTag();
-        }
-       /*
-        CompanionItem rowItem = results.get(position);
-        	holder.txtName.setText(rowItem.getName());
-        	holder.txtNameSub.setText(rowItem.getGender() + " " + rowItem.getRace());
-        	holder.txtRole.setText("Role: " + rowItem.getRole());
-        	holder.txtBonus.setText(rowItem.getBonus());
-        	holder.txtRomance.setText("Romanceable: " + rowItem.getRomance());
-        	holder.txtStats.setText("Stats: " + rowItem.getPrimaryStat() + ", " + rowItem.getSecondaryStat());
-        	holder.txtWeapon.setText("Weapons: " + rowItem.getPrimaryWeapon() + ", " + rowItem.getSecondaryWeapon());
-        	holder.txtDescription.setText(rowItem.getDescription());
-        	holder.txtArmor.setText("Armor: " + rowItem.getArmor());
-        */
-        return v;
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+        View view = View.inflate(viewGroup.getContext(), R.layout.companion_row, null);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
-    private static class ViewHolder {
-        public TextView txtBonus;
-        public TextView txtNameSub;
-        public TextView txtGifts;
-        public TextView txtArmor;
-        public TextView txtName;
-        public TextView txtStats;
-        public TextView txtWeapon;
-        public TextView txtRole;
-        public TextView txtRomance;
-        public TextView txtDescription;
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        CompanionItem companionRow = companionItem.get(position);
+        viewHolder.comName.setText(companionRow.getName());
+        viewHolder.comTitle.setText(companionRow.get);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != companionItem ? companionItem.size() : 0);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView comName;
+        public TextView comTitle;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            comName = (TextView) itemView.findViewById(R.id.comName);
+            comTitle = (TextView) itemView.findViewById(R.id.comTitle);
+        }
     }
 }
